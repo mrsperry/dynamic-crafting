fd5395d2ce9982ccd8e4949d25bbe6fdb82dae44
Josh <mrjoshuasperry@gmail.com>
2018-03-22 20:20:27 -0400

    Add initial files

---
+++ src/main/java/io/github/mrsperry/dynamiccrafting/Main.java
@@ -0,0 +1,15 @@
+package io.github.mrsperry.dynamiccrafting;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        this.saveDefaultConfig();
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
